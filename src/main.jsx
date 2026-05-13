5df075f32afabda902296aa89bed7fd77d77c660
gautamdevani14 <gautamdevani14@gmail.com>
2026-05-13 18:55:17 +0530

    Setup GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from "./store/store.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/movie-project">
       <App />
     </BrowserRouter>
   </Provider>
